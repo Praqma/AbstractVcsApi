f49389a27ef3f77e88d38e9ec27af8351cc08768
wolfgarnet <wolfgarnet@gmail.com>
2011-08-14 16:47:12 +0200

    Added new test

---
+++ src/main/java/net/praqma/vcs/test/GitToClearcase2.java
@@ -0,0 +1,100 @@
+package net.praqma.vcs.test;
+
+import java.io.File;
+import java.util.List;
+
+import net.praqma.clearcase.PVob;
+import net.praqma.clearcase.Vob;
+import net.praqma.clearcase.ucm.UCMException;
+import net.praqma.clearcase.ucm.entities.Baseline;
+import net.praqma.clearcase.ucm.entities.Project;
+import net.praqma.clearcase.ucm.entities.Stream;
+import net.praqma.clearcase.ucm.entities.UCM;
+import net.praqma.clearcase.ucm.entities.UCMEntity;
+import net.praqma.exceptions.ElementDoesNotExistException;
+import net.praqma.exceptions.ElementNotCreatedException;
+import net.praqma.exceptions.UnableToReplayException;
+import net.praqma.util.debug.Logger;
+import net.praqma.util.debug.Logger.LogLevel;
+import net.praqma.vcs.AVA;
+import net.praqma.vcs.model.AbstractCommit;
+import net.praqma.vcs.model.clearcase.ClearcaseBranch;
+import net.praqma.vcs.model.clearcase.ClearcaseReplay;
+import net.praqma.vcs.model.clearcase.ClearcaseVCS;
+import net.praqma.vcs.model.git.GitBranch;
+import net.praqma.vcs.util.Utils;
+
+public class GitToClearcase2 {
+	static Logger logger = Logger.getLogger();
+	
+	public static void main( String[] args ) throws UCMException, ElementNotCreatedException, ElementDoesNotExistException, UnableToReplayException {
+		
+		if( args.length < 4 ) {
+			System.err.println( "Too few parameters" );
+			System.err.println( "Usage: GitToClearcase <vobname> <componentname> <viewroot> <append>" );
+			System.exit( 1 );
+		}
+		
+		new AVA();
+		logger.setMinLogLevel( LogLevel.INFO );
+		
+		String vname = args[0];
+		String append = args[3];
+		File path = new File( args[2] + append );
+		
+		Vob vob = new Vob( "\\" + vname );
+		String name = vname + "_" + append;
+		
+		/* Do the ClearCase thing... */
+		UCM.setContext( UCM.ContextType.CLEARTOOL );
+		
+		logger.toStdOut( true );
+		
+		PVob pvob = ClearcaseVCS.bootstrap();
+		Stream parent = UCMEntity.getStream( "Development_int", pvob, true );
+		Baseline baseline = UCMEntity.getBaseline( "Structure_1_0", pvob, true );
+
+		
+		ClearcaseVCS cc = ClearcaseVCS.create( null, vname, Project.POLICY_INTERPROJECT_DELIVER  | 
+                                                                   Project.POLICY_CHSTREAM_UNRESTRICTED | 
+                                                                   Project.POLICY_DELIVER_NCO_DEVSTR, pvob );
+		
+		
+		ClearcaseBranch ccbranch = new ClearcaseBranch( cc, vob, parent, baseline, path, name + "_view", name + "_dev" );
+		ccbranch.get();
+		ccbranch.checkout();
+
+		ClearcaseReplay cr = new ClearcaseReplay( ccbranch );
+		
+		GitBranch branch = new GitBranch( new File( "C:/projects/monkit/branches" ), "master" );
+		List<AbstractCommit> commits = branch.getCommits(true);
+		
+		logger.info( commits.size() + " commits on branch " + branch );
+		
+		logger.info( "Commit #1: " + commits.get( 0 ) );
+
+		for( int i = 0 ; i < commits.size() ; i++ ) {
+			System.out.print( "\r" + Utils.getProgress( commits.size(), i ) );
+			System.out.print( " - " + commits.get( i ).getTitle() );
+			//branch.checkout( commits.get( i ) );
+			cr.replay( commits.get( i ) );
+		}
+		System.out.println(" Done");
+		
+		/* For jenkins */
+		logger.info( "Creating special Jenkins project" );
+		Project jenkinsProject = null;
+		try {
+			jenkinsProject = Project.create( "jenkins", null, pvob, Project.POLICY_INTERPROJECT_DELIVER | Project.POLICY_CHSTREAM_UNRESTRICTED | Project.POLICY_DELIVER_NCO_DEVSTR, "Development project", baseline.getComponent() );
+		} catch (UCMException e) {
+			logger.error("Error while creating Jenkins Project: " + e.getMessage());
+		}
+		
+		logger.info("Creating Jenkins integration stream");
+		try {
+			Stream jenkinsIntStream = Stream.createIntegration( "jenkins_int", jenkinsProject, baseline );
+		} catch (UCMException e) {
+			logger.error("Error while creating Jenkins Integratiom Stream: " + e.getMessage());
+		}
+	}
+}
