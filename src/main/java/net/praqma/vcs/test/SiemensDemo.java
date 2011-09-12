d080b4fec8ade4e4ea5505cf77793b5459db2c14
wolfgarnet <wolfgarnet@gmail.com>
2011-09-13 01:17:00 +0200

    Fixed demo

--- src/main/java/net/praqma/vcs/test/SiemensDemo.java
+++ src/main/java/net/praqma/vcs/test/SiemensDemo.java
@@ -21,8 +21,10 @@ import net.praqma.util.debug.Logger;
 import net.praqma.util.debug.Logger.LogLevel;
 import net.praqma.util.option.Option;
 import net.praqma.util.option.Options;
+import net.praqma.vcs.AVA;
 import net.praqma.vcs.model.AbstractCommit;
 import net.praqma.vcs.model.clearcase.ClearcaseBranch;
+import net.praqma.vcs.model.clearcase.ClearcaseReplay;
 import net.praqma.vcs.model.clearcase.ClearcaseVCS;
 import net.praqma.vcs.model.exceptions.ElementAlreadyExistsException;
 import net.praqma.vcs.model.exceptions.ElementDoesNotExistException;
@@ -35,11 +37,14 @@ import net.praqma.vcs.model.git.GitReplay;
 
 /**
  * INIT:<br>
- * java -classpath ava.jar net.praqma.vcs.test.SiemensDemo -w c:\Temp\views\siemens\001 -t siemens_view_tag -o Siemens -c Siemens -s Siemens_int -p siemens -g c:\projects\monkit\branches -V
+ * java -classpath ava.jar net.praqma.vcs.test.SiemensDemo -w c:\Temp\views\siemens_out\001 -t siemens_out_view_tag -o Siemens -c Siemens -S Siemens_int -p siemens -g c:\projects\other\Siemens -V
+ * java -classpath ava.jar net.praqma.vcs.test.SiemensDemo -w c:\Temp\views\siemens_in\001 -t siemens_in_view_tag -o Siemens -c Siemens -S Siemens_int -p siemens -g c:\projects\other\Siemens -V
  * <br>
  * 
  * RUN:<br>
- * java -classpath ava.jar net.praqma.vcs.test.SiemensDemo -w c:\Temp\views\siemens\001 -t siemens_view_tag -o Siemens -b Siemens_Structure_1_0 -s Siemens_int -p siemens -g c:\projects\monkit\branches -V
+ * java -classpath ava.jar net.praqma.vcs.test.SiemensDemo -G -w c:\Temp\views\siemens_out\001 -t siemens_out_view_tag -o Siemens -b Siemens_Structure_1_0 -S Siemens_int -s siemens_out_dev -g c:\projects\other\Siemens -V
+ * java -classpath ava.jar net.praqma.vcs.test.SiemensDemo -w c:\Temp\views\siemens_in\001 -t siemens_in_view_tag -o Siemens -b Siemens_Structure_1_0 -S Siemens_int -s siemens_in_dev -g c:\projects\other\Siemens -V
+ * 
  * 
  * @author wolfgang
  *
@@ -61,9 +66,11 @@ public class SiemensDemo {
         Option oviewtag = new Option( "viewtag", "t", true, 1, "Viewtag name" );
         Option ovobname = new Option( "vob", "o", true, 1, "Vob name" );
         Option ocomponent = new Option( "component", "c", false, 1, "Component name" );
-        Option ostreamname = new Option( "stream", "s", true, 1, "Parent stream name" );
-        Option oprojectname = new Option( "project", "p", true, 1, "Project name" );
+        Option ostreamname = new Option( "stream", "S", true, 1, "Parent stream name" );
+        Option ochildstreamname = new Option( "childstream", "s", false, 1, "Child stream name" );
+        Option oprojectname = new Option( "project", "p", false, 1, "Project name" );
         Option obaselinename = new Option( "baseline", "b", false, 1, "Foundation Baseline name" );
+        Option otogit = new Option( "togit", "G", false, 0, "The to Git solution" );
         Option ogit = new Option( "git", "g", true, 1, "Path to git repo" );
         
         o.setOption( oview );
@@ -71,9 +78,11 @@ public class SiemensDemo {
         o.setOption( ovobname );
         o.setOption( ocomponent );
         o.setOption( ostreamname );
+        o.setOption( ochildstreamname );
         o.setOption( oprojectname );
         o.setOption( ogit );
         o.setOption( oviewtag );
+        o.setOption( otogit );
         
         o.setDefaultOptions();
         
@@ -102,6 +111,8 @@ public class SiemensDemo {
 		plogger.setLocalLog(new File("demo.log"));
 		Cool.setLogger( plogger );
 		
+		new AVA();
+		
 		/* Setup ClearCase */
 		PVob pvob = ClearcaseVCS.bootstrap();
 		
@@ -114,21 +125,27 @@ public class SiemensDemo {
 			
 			cc.get(true);
 			logger.info( "Clearcase initialized" );
-
+			return;
 			
-			baseline = cc.getInitialBaseline();
 		} else {
 			baseline = UCMEntity.getBaseline( obaselinename.getString(), pvob, false );
+		}
+		
+		if( !ochildstreamname.isUsed() ) {
+			logger.error( "The child stream must be given" );
+			System.exit( 1 );
 		}
 		
 		Vob vob = new Vob( "\\" + ovobname.getString() );
 		Stream stream = UCMEntity.getStream( ostreamname.getString(), pvob, false );
 		
-		ClearcaseBranch ccbranch = new ClearcaseBranch( pvob, vob, stream, baseline, new File( oview.getString() ), oviewtag.getString(), oprojectname.getString() + "_dev" );
+		ClearcaseBranch ccbranch = new ClearcaseBranch( pvob, vob, stream, baseline, new File( oview.getString() ), oviewtag.getString(), ochildstreamname.getString() );
 		ccbranch.initialize(true);
+		ccbranch.update();
 		
 		/* Setup Git */
 		GitBranch gitbranch = new GitBranch( new File( ogit.getString() ), "master" );
+		gitbranch.update();
 		
         MyShutdown sh = new MyShutdown();
         Runtime.getRuntime().addShutdownHook(sh);
@@ -137,15 +154,29 @@ public class SiemensDemo {
         Date now = null;
         
         GitReplay gr = new GitReplay( gitbranch );
+        ClearcaseReplay cr = new ClearcaseReplay( ccbranch );
 		
 		while( true ) {
 			if( now != null ) {
 				logger.info( "Getting baselines after " + now );
 			}
-			List<AbstractCommit> cccommits = ccbranch.getCommits(true, now);
-			for( AbstractCommit commit : cccommits ) {
-				ccbranch.checkoutCommit( commit );
-				gr.replay( commit );
+			
+			/* To Git solution */
+			if( otogit.isUsed() ) {
+				logger.info( "Getting ClearCase commits" );
+				List<AbstractCommit> cccommits = ccbranch.getCommits(true, now);
+				for( AbstractCommit commit : cccommits ) {
+					ccbranch.checkoutCommit( commit );
+					gr.replay( commit );
+				}
+			/* To clearcase solution */
+			} else {
+				logger.info( "Getting Git commits" );
+				List<AbstractCommit> gitcommits = gitbranch.getCommits(true, now);
+				for( AbstractCommit commit : gitcommits ) {
+					gitbranch.checkoutCommit( commit );
+					cr.replay( commit );
+				}
 			}
 			
 			now = new Date();
