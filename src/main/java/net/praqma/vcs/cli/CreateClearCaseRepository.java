f8d8e857449955eb4369fd13b7f1ec2ea3bbd6fa
wolfgarnet <wolfgarnet@gmail.com>
2011-09-20 14:46:02 +0200

    Added cli

---
+++ src/main/java/net/praqma/vcs/cli/CreateClearCaseRepository.java
@@ -0,0 +1,83 @@
+package net.praqma.vcs.cli;
+
+import java.io.IOException;
+
+import net.praqma.clearcase.PVob;
+import net.praqma.clearcase.ucm.UCMException;
+import net.praqma.clearcase.ucm.entities.Baseline;
+import net.praqma.clearcase.ucm.entities.Project;
+import net.praqma.clearcase.ucm.entities.UCM;
+import net.praqma.util.debug.Logger;
+import net.praqma.util.debug.Logger.LogLevel;
+import net.praqma.util.debug.appenders.StreamAppender;
+import net.praqma.util.option.Option;
+import net.praqma.util.option.Options;
+import net.praqma.vcs.AVA;
+import net.praqma.vcs.model.clearcase.ClearcaseVCS;
+import net.praqma.vcs.model.exceptions.ElementAlreadyExistsException;
+import net.praqma.vcs.model.exceptions.ElementDoesNotExistException;
+import net.praqma.vcs.model.exceptions.ElementNotCreatedException;
+import net.praqma.vcs.model.exceptions.UnableToCheckoutCommitException;
+import net.praqma.vcs.model.exceptions.UnableToReplayException;
+
+public class CreateClearCaseRepository {
+	private static Logger logger = Logger.getLogger();
+	private static StreamAppender app = new StreamAppender( System.out );
+	
+    static class MyShutdown extends Thread {
+        public void run() {
+            System.out.println( "Terminating AVA process" );
+        }
+    }
+	
+	public static void main(String[] args) throws IOException, UCMException, ElementNotCreatedException, ElementAlreadyExistsException, ElementDoesNotExistException, UnableToCheckoutCommitException, UnableToReplayException {
+		
+        Options o = new Options( "1.0.0" );
+
+        Option ovobname = new Option( "vob", "o", true, 1, "Vob name" );
+        Option ocomponent = new Option( "component", "c", true, 1, "Component name" );
+        Option ostreamname = new Option( "stream", "s", true, 1, "Parent stream name" );
+        Option oprojectname = new Option( "project", "p", true, 1, "Project name" );
+        
+        o.setOption( ovobname );
+        o.setOption( ocomponent );
+        o.setOption( ostreamname );
+        o.setOption( oprojectname );
+        
+        o.setDefaultOptions();
+        
+        o.parse( args );
+        
+        Logger.addAppender( app );
+
+        try {
+            o.checkOptions();
+        } catch( Exception e ) {
+        	logger.error( "Incorrect option: " + e.getMessage() );
+            o.display();
+            System.exit( 1 );
+        }
+
+        if( o.isVerbose() ) {
+        	app.setMinimumLevel( LogLevel.DEBUG );
+        } else {
+        	app.setMinimumLevel( LogLevel.INFO );
+        }
+        
+		/* Do the ClearCase thing... */
+		UCM.setContext( UCM.ContextType.CLEARTOOL );
+		
+		new AVA();
+		
+		/* Setup ClearCase */
+		PVob pvob = ClearcaseVCS.bootstrap();
+		
+		ClearcaseVCS cc = new ClearcaseVCS( null, ovobname.getString(), ocomponent.getString(), oprojectname.getString(), ostreamname.getString(), 
+                Project.POLICY_INTERPROJECT_DELIVER  | 
+                Project.POLICY_CHSTREAM_UNRESTRICTED | 
+                Project.POLICY_DELIVER_NCO_DEVSTR, pvob );
+		
+		cc.get(true);
+		logger.info( "Clearcase initialized" );
+	}
+}
