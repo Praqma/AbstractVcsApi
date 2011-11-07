fda0df0cd29fa5f38b51943b3d7184a882992fcd
wolfgarnet <wolfgarnet@gmail.com>
2011-11-07 09:52:49 +0100

    Added vcs check

---
+++ src/main/java/net/praqma/vcs/util/VCS.java
@@ -0,0 +1,27 @@
+package net.praqma.vcs.util;
+
+import java.io.File;
+
+import net.praqma.vcs.VersionControlSystems;
+
+public abstract class VCS {
+	public static VersionControlSystems determineVCS( File path ) {
+		VersionControlSystems vcs = VersionControlSystems.Unknown;
+		
+		File cc = new File( path, "view.dat" );
+		File hg = new File( path, ".hg" );
+		File git = new File( path, ".git" );
+		/* Check ClearCase */
+		if( cc.exists() && cc.isFile() ) {
+			return VersionControlSystems.ClearCase;
+		/* Check Mercurial */
+		} else if( hg.exists() && hg.isDirectory() ) {
+			return VersionControlSystems.Mercurial;
+		/* Check Git */
+		} else if( git.exists() && git.isDirectory() ) {
+			return VersionControlSystems.Git;
+		}
+		
+		return vcs;
+	}
+}
